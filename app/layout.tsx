b7e7a22cc652e0aa44163315beeaf78bced51183
rakesh_raja <rakeshraja@sightspectrum.com>
2024-07-09 14:41:20 +0530

    script again fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,29 @@
-import React, { ReactNode } from "react";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Head from "next/head";
+import "./globals.css";
+import { Providers } from "./providers";
+import Header from "./components/Navbar";
+import FloatingButton from "./components/FloatingButton";
+// import "../public/assets/js/main"
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
-import {Providers} from "./providers";
-import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
-type RootLayoutProps = {
-  children: ReactNode;
+export const metadata: Metadata = {
+  title: "The SkyView Rooftop Multicuisine Restaurant",
+  description: "Elevate Your Dining Experience",
 };
 
-const RootLayout = ({ children }: RootLayoutProps) => {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <>
-      <Head>
+    <html lang="en" className="dark">
+      <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="google-site-verification"
@@ -25,24 +32,18 @@ const RootLayout = ({ children }: RootLayoutProps) => {
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/swiper@6/swiper-bundle.min.css"
+          href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
         />
-      </Head>
-      <body className="dark">
+        <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js" async></script>
+      </head>
+      <body>
+        {/* <FloatingButton /> */}
+        {/* <Example /> */}
         <Navbar />
         <Providers>{children}</Providers>
         <SpeedInsights />
         <Analytics />
-        <Script src="https://cdn.jsdelivr.net/npm/swiper@6/swiper-bundle.min.js" async></Script>
-
       </body>
-    </>
+    </html>
   );
-};
-
-export const metadata = {
-  title: "The SkyView Rooftop Multicuisine Restaurant",
-  description: "Elevate Your Dining Experience",
-};
-
-export default RootLayout;
+}
