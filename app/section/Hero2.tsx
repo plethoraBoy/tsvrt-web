4c7ddc316764275b9f456e7279a6f4506e8edc6d
rakesh_raja <rakeshraja@sightspectrum.com>
2024-07-09 14:59:49 +0530

    fix hero restaurant name

--- app/section/Hero2.tsx
+++ app/section/Hero2.tsx
@@ -67,7 +67,7 @@ const HeroSection: React.FC = () => {
               className={`${styles["heading-style-2"]} 
              `}
             >
-              The SkyView Roof Multicuisine Restaurant
+              The SkyView Rooftop Multicuisine Restaurant{" "}
             </h2>
             {/* <div className="flex justify-center items-center px-4">
               <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
