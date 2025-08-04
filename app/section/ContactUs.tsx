6537e9f3d6cd15397a6491f05f3bdf07d6fd95ad
plethoraBoy <rockyrjunior@gmail.com>
2025-08-05 01:12:52 +0530

    apos lint

--- app/section/ContactUs.tsx
+++ app/section/ContactUs.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
-import React, { useState, useEffect, FormEvent, ChangeEvent } from "react";
+import React, { useState, FormEvent, ChangeEvent } from "react";
 import { motion } from "framer-motion";
 
 const fadeUpVariant = {
@@ -46,29 +47,29 @@ const ContactSection: React.FC = () => {
       className="py-16 relative overflow-hidden font-inter"
     >
       {/* Food-themed background elements */}
-      <div 
+      <div
         className="absolute inset-0 z-0"
         style={{
           backgroundImage: `url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 32c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%23d4a76a' fill-opacity='0.03' fill-rule='evenodd'/%3E%3C/svg%3E")`,
-          backgroundSize: "300px 300px"
+          backgroundSize: "300px 300px",
         }}
       />
-      
+
       {/* Subtle food pattern */}
       <div className="absolute inset-0 z-0 opacity-5">
-        <div 
+        <div
           className="absolute top-1/4 left-1/4 w-48 h-48 rounded-full border-2 border-[#d4a76a] blur-xl"
-          style={{ 
+          style={{
             background: "radial-gradient(circle, #ca8e24 0%, transparent 70%)",
-            transform: "rotate(15deg)"
-          }} 
+            transform: "rotate(15deg)",
+          }}
         />
-        <div 
+        <div
           className="absolute bottom-1/4 right-1/4 w-64 h-64 rounded-full border-2 border-[#d4a76a] blur-xl"
-          style={{ 
+          style={{
             background: "radial-gradient(circle, #d4a76a 0%, transparent 70%)",
-            transform: "rotate(-10deg)"
-          }} 
+            transform: "rotate(-10deg)",
+          }}
         />
       </div>
 
@@ -84,7 +85,8 @@ const ContactSection: React.FC = () => {
             Get in Touch
           </h2>
           <p className="text-gray-300 text-lg">
-            Have a question about our menu or reservations? We'd love to hear from you!
+            Have a question about our menu or reservations? We'd love to hear
+            from you!
           </p>
         </motion.div>
 
@@ -153,7 +155,11 @@ const ContactSection: React.FC = () => {
             />
             <div className="md:col-span-2 text-center text-[#d4a76a] min-h-[24px] text-sm md:text-base">
               {isSubmitting && <span>Preparing your message...</span>}
-              {isSent && <span className="text-[#ca8e24]">Your message has been sent! We'll respond within 24 hours.</span>}
+              {isSent && (
+                <span className="text-[#ca8e24]">
+                  Your message has been sent! We'll respond within 24 hours.
+                </span>
+              )}
               {errorMsg && <span className="text-rose-400">{errorMsg}</span>}
             </div>
             <button
